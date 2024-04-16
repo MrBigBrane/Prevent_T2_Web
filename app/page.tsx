acc73d67d6f1d2cd1990450f0f62cd28b82d43ce
Aniruddh Doki <doki.aniruddh@gmail.com>
2024-04-16 14:36:44 -0400

    added a new loader to mark code for dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-'use server';
-
+"use server";
 
 export default async function Index() {
-  
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      
-          <h1>Dummy Text</h1>
-
+      {/* START_DEV_ONLY */}
+      <h1>Dummy Text</h1>
+      {/* END_DEV_ONLY */}
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const path = require("path");
+
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: "pre",
+          exclude: /(node_modules|bower_components|\.spec\.js)/,
+          use: [{ loader: path.resolve("loaders/strip-code-loader.js") }],
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
