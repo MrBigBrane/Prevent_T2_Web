0ff14f9a17d75b20fbb50299b15c5c0027add9d3
Aniruddh Doki <doki.aniruddh@gmail.com>
2024-04-28 22:37:15 -0400

    using sql functions for minutes per week

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,16 @@
-'use server';
+"use server";
 
-import MuiModal from '@/components/forms/MuiModal'
-import LinkButton from '@/components/buttons/LinkButton';
-import MinutesGraph from '@/components/graph/MinutesGraph';
-import WeightGraph from '@/components/graph/WeightGraph';
-import minutesPerWeek from '@/components/serverfunctions/minutesPerWeek'
-import TableViewIcon from '@mui/icons-material/TableView';
+import MuiModal from "@/components/forms/MuiModal";
+import LinkButton from "@/components/buttons/LinkButton";
+import MinutesGraph from "@/components/graph/MinutesGraph";
+import WeightGraph from "@/components/graph/WeightGraph";
+import minutesPerWeek from "@/components/serverfunctions/minutesPerWeek";
+import TableViewIcon from "@mui/icons-material/TableView";
 
 export default async function Index() {
-  let minutesData = Array.from(await minutesPerWeek())
-  const weeksMinutes = minutesData[0][minutesData[0].length - 1]
+  let minutesData = Array.from(await minutesPerWeek());
+  console.log(minutesData);
+  const weeksMinutes = minutesData; // [0][minutesData[0].length - 1]
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -18,29 +19,51 @@ export default async function Index() {
       <h1>Exercise Minutes</h1>
       <MinutesGraph />
       <div>
-          <h1 style={{
-            float: 'left'
-          }}>
-            Activity Logs</h1>
-          <MuiModal edit={false} title='Activity Logger' rowId={false} field1='' field2='' field3='' />
-          <LinkButton href="dashboard/activities" 
-            type={null}
-            label="View Activities"
-            startIcon={<TableViewIcon />}
-          />
-      </div>
-      <div>
-        <h1 style={{
-          float: "left"
-        }}>Lifestyle Logs</h1>
-        <MuiModal edit={false} title={null} rowId={false} field1='' field2='' field3={weeksMinutes} />
-        <LinkButton href="dashboard/coachlog"
-        type={null}
-        label="View Lifestyle Log"
-        startIcon={<TableViewIcon/>}
+        <h1
+          style={{
+            float: "left",
+          }}
+        >
+          Activity Logs
+        </h1>
+        <MuiModal
+          edit={false}
+          title="Activity Logger"
+          rowId={false}
+          field1=""
+          field2=""
+          field3=""
+        />
+        <LinkButton
+          href="dashboard/activities"
+          type={null}
+          label="View Activities"
+          startIcon={<TableViewIcon />}
         />
       </div>
-      
+      <div>
+        <h1
+          style={{
+            float: "left",
+          }}
+        >
+          Lifestyle Logs
+        </h1>
+        <MuiModal
+          edit={false}
+          title={null}
+          rowId={false}
+          field1=""
+          field2=""
+          field3={weeksMinutes}
+        />
+        <LinkButton
+          href="dashboard/coachlog"
+          type={null}
+          label="View Lifestyle Log"
+          startIcon={<TableViewIcon />}
+        />
+      </div>
     </div>
   );
 }
