5fd0738e6a5356e8d6f60db99d36d7ed5dfb60e2
MrBigBrane <158606917+MrBigBrane@users.noreply.github.com>
2024-05-16 13:10:26 -0700

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,14 +1,11 @@
 'use server';
 
-import MuiModal from '@/components/forms/userforms/MuiModal'
-import LinkButton from '@/components/buttons/LinkButton';
 import MinutesGraph from '@/components/graph/MinutesGraph';
-import WeightGraph from '@/components/graph/WeightGraph';
-import minutesPerWeek from '@/components/serverfunctions/minutesPerWeek'
-import TableViewIcon from '@mui/icons-material/TableView';
 import MuiSuccess from '@/components/buttons/alerts/MuiSuccess'
 import getCurrentUser from '@/components/serverfunctions/getCurrentUser';
 import { redirect } from 'next/navigation';
+import { Box, Paper, Typography } from '@mui/material';
+import MuiDrawer from '@/components/navigation/userdashboard/MuiDrawer'
 
 export default async function Index({ searchParams }) {
   const user = Object.assign({}, await getCurrentUser())
@@ -16,40 +13,37 @@ export default async function Index({ searchParams }) {
       redirect('/login?message=Please login before trying to access user data.')
     }
 
-  let minutesData = Array.from(await minutesPerWeek())
-  let weeksMinutes = minutesData[1][minutesData[1].length - 1].toString()
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <>
       {searchParams?.form &&  <MuiSuccess severity="success">Form Submitted!</MuiSuccess>}
-      <h1>Weight Progress</h1>
-      <WeightGraph />
-      <h1>Exercise Minutes</h1>
-      <MinutesGraph />
-      <div>
-          <h1 style={{
-            float: 'left'
-          }}>
-            Activity Logs</h1>
-          <MuiModal edit={false} title='Activity Logger' rowId={false} field1='' field2='' field3='' />
-          <LinkButton href="dashboard/activities" 
-            type={null}
-            label="View Activities"
-            startIcon={<TableViewIcon />}
+      <h1>Minute Stats</h1>
+        <Box width={"100%"}>
+          <MuiDrawer
+            main={
+              <Box
+                sx={{
+                  width: "100%",
+                  textAlign: "center",
+                  padding: "20px",
+                  bottompadding: "0px",
+                }}
+              >
+                <Paper elevation={10}>
+                  <Typography
+                    variant="h6"
+                    component="h2"
+                    sx={{ padding: "20px" }}
+                  >
+                    Minute Progress
+                  </Typography>
+                  <MinutesGraph />
+                </Paper>
+              </Box>
+            }
           />
-      </div>
-      <div>
-        <h1 style={{
-          float: "left"
-        }}>Lifestyle Logs</h1>
-        <MuiModal edit={false} title={null} rowId={false} field1='' field2='' field3={weeksMinutes} />
-        <LinkButton href="dashboard/coachlog"
-        type={null}
-        label="View Lifestyle Log"
-        startIcon={<TableViewIcon/>}
-        />
-      </div>
+        </Box>
       
-    </div>
+    </>
   );
 }
