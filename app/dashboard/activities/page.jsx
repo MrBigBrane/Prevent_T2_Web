c264165c959ac426dd4055fddff89f1970296ddd
MrBigBrane <158606917+MrBigBrane@users.noreply.github.com>
2024-05-16 17:09:21 -0700

    Update page.jsx

--- app/dashboard/activities/page.jsx
+++ app/dashboard/activities/page.jsx
@@ -1,3 +1,9 @@
+'use server';
+
+import ActivityTable from "../../../components/tables/ActivityTable";
+import MuiModal from '../../../components/forms/MuiModal'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import LinkButton from "@/components/buttons/LinkButton";
 import MuiSuccess from '@/components/buttons/alerts/MuiSuccess'
 import getCurrentUser from '@/components/serverfunctions/getCurrentUser'
 import { redirect } from "next/navigation";
@@ -5,6 +11,14 @@ import { Box } from "@mui/material";
 
 
 export default async function ActivityTablePage({ searchParams }) {
+  // Fetch the current user from the server
+  const user = Object.assign({}, await getCurrentUser())
+
+  // If the user is not logged in, redirect them to the login page
+  if (!user.id) {
+    redirect('/login?message=Please login before trying to access user data.')
+  }
+
   return (
     <>
       {/* Render success messages for add, edit, and delete actions */}
@@ -29,7 +43,7 @@ export default async function ActivityTablePage({ searchParams }) {
       />
 
       {/* Render the Activity Logs page header */}
-
+      
 
       {/* Render the Activity Logger modal component */}
       <MuiModal
