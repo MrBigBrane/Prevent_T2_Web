f2f02861387719ef4186a706882663defb9600ce
MrBigBrane <158606917+MrBigBrane@users.noreply.github.com>
2024-05-16 17:08:31 -0700

    Update page.jsx

--- app/dashboard/coachlog/page.jsx
+++ app/dashboard/coachlog/page.jsx
@@ -1,12 +1,13 @@
 'use server';
 
-import MuiModal from '@/components/forms/userforms/MuiModal'
+import MuiModal from '@/components/forms/MuiModal'
 import LinkButton from '@/components/buttons/LinkButton';
-import CoachTable from '@/components/tables/users/CoachTable';
+import CoachTable from '@/components/tables/CoachTable';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import minutesPerWeek from '@/components/serverfunctions/minutesPerWeek'
 import MuiSuccess from '@/components/buttons/alerts/MuiSuccess'
 import getCurrentUser from '@/components/serverfunctions/getCurrentUser';
+import { Box } from '@mui/material';
 
 export default async function CoachPage({ searchParams }) {
   const user = Object.assign({}, await getCurrentUser())
@@ -24,10 +25,45 @@ export default async function CoachPage({ searchParams }) {
         {searchParams?.add &&  <MuiSuccess severity="success">Coach Log Added!</MuiSuccess>}
         {searchParams?.edit &&  <MuiSuccess severity="success">Coach Log Edited!</MuiSuccess>}
         {searchParams?.delete &&  <MuiSuccess severity="success">Coach Log Deleted!</MuiSuccess>}
-        <LinkButton href="/dashboard" label="Back" type={null} startIcon={<ArrowBackIcon />} />
-        <h1>Coaching Log</h1>
-        <MuiModal edit={false} title={null} rowId={false} field3={weeksMinutes} field2='' field1='' />
-        <CoachTable table="lifestyle_coach_log" />
+        
+        
+        
+          <LinkButton 
+          href="/dashboard/weightstats" 
+          label="Back" 
+          type={null} 
+          startIcon={<ArrowBackIcon />} 
+          padding={2}
+          style={{position : 'fixed', left: '1rem', top: '5rem'}}
+        />
+        <MuiModal 
+          edit={false} 
+          title={null} 
+          rowId={false} 
+          field3={weeksMinutes} 
+          field2='' 
+          field1='' 
+          style={{position : 'fixed', right: '1rem', top: '5rem'}}
+          search={searchParams?.open ? true : false}
+          // sx={{
+          //   display: 'grid',
+          //   justifyContent: 'end',
+          //   alignItems: 'center',
+          //   height: '100vh',
+          //   paddingRight: 2
+          // }}
+        />
+        <h1 style={{ marginTop: '1.5rem'}}>Coaching Log</h1>
+        <Box 
+        style={{width: '95%', textAlign: 'center'}}
+         marginTop={4}
+         marginLeft={1}
+         marginRight={1}>
+          
+          <CoachTable table="lifestyle_coach_log" />
+        </Box>
+        
+        
       </>
         
     )
