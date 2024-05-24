150bf703c874dd15c573e72c7530440919a9ab3f
VarunThota0954 <varunt11@outlook.com>
2024-05-24 11:42:02 -0700

    Ryan Help!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,12 @@
 'use server';
 
 import LinkButton from '@/components/buttons/LinkButton';
-import { Box, Paper, Typography, Grid } from '@mui/material';
+import { Box, Paper, Typography, Grid, colors } from '@mui/material';
 import MuiAvatar from '@/components/buttons/avatar/MuiAvatar';
 import EmailReset from '../../components/forms/reset/EmailReset';
 import PasswordReset from '../../components/forms/reset/PasswordReset';
 import { createClient } from "@/utils/supabase/server";
+import { Margin } from '@mui/icons-material';
 
 export default async function ProfilePage() {
     const supabase = createClient();
@@ -18,34 +19,44 @@ export default async function ProfilePage() {
     <>
         <h1>Profile</h1>
         <Paper square={false} elevation={3} style={{ width: '45%' }}>
-            <MuiAvatar name={`${user.user_metadata.first_name} ${user.user_metadata.last_name}`} sx={{ width: 32, height: 32 }}/>
             <Box>
-                {/* <MuiAvatar name={null} sx={{ width: 32, height: 32 }}/> */}
-                <Typography variant='h4'>{user.user_metadata.first_name} {user.user_metadata.last_name}</Typography>
+                <Grid container>
+                    <Grid item>
+                        <MuiAvatar name={`${user.user_metadata.first_name} ${user.user_metadata.last_name}`} style={{ width: 90, height: 90 }} sizes="large"/>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant='h4'>{user.user_metadata.first_name} {user.user_metadata.last_name}</Typography>
+                    </Grid>
+                </Grid>
                 <EmailReset />
             </Box>
         </Paper>
-        <h1>Forms</h1>
-        <Box>
-            <Grid container>
-                <Grid item>
+        <Box style={{ width: '45%' }} marginTop={5}>
+            <Typography variant='h4'>Forms</Typography>
+            <Grid container spacing={3} justifyContent={'center'} padding={1}>
+                <Grid item xs={6}>
                     <Paper>
-                        <Typography variant='h5'>Description</Typography>
-                        <LinkButton href="/profile/onboarding?section1=true" startIcon={null} label="Onboarding Form" type='' />
+                        <Typography variant='h5'>Onboarding Form</Typography>
+                        <Typography variant='body2'>Description</Typography>
+                        <LinkButton href="/profile/onboarding?section1=true" startIcon={null} label="Onboarding Form" type='' style={{ Margin : '2rem' }} />
                     </Paper>
                 </Grid>
-                <Grid item>
+                <Grid item xs={6}>
                     <Paper>
-                        <Typography variant='h5'>Description</Typography>
-                        <LinkButton href="/profile/becomecoach" startIcon={null} label="Become a Coach" type='' />
+                        <Typography variant='h5'>Coach Registration Form</Typography>
+                        <Typography variant='body2'>Description</Typography>
+                        <LinkButton href="/profile/becomecoach" startIcon={null} label="Become a Coach" type='' style={{ Margin : 2 }} />
                     </Paper>
                 </Grid>
                 
             </Grid>
             
         </Box>
-        <h1>Password Change</h1>
-        <PasswordReset />
+        <Box style={{ width: '45%' }} marginTop={5}>
+            <Typography variant='h4'>Your Password</Typography>
+            <Typography variant='body2'>Change Password</Typography>
+            <PasswordReset />
+        </Box>
     </>
     )
 }
--- components/buttons/LinkButton.jsx
+++ components/buttons/LinkButton.jsx
@@ -1,10 +1,10 @@
 import MuiButton from './MuiButton'
 import Link from "next/link";
 
-export default function LinkButton({ href, startIcon, label, type, variant, style }) {
+export default function LinkButton({ href, startIcon, label, type, variant, ...props }) {
     return (
-      <Link href={href}>
-        <MuiButton variant = {variant} type={type} color={"success"} label={label} startIcon={startIcon} style={style} />
+      <Link href={href} {...props}>
+        <MuiButton variant = {variant} type={type} color={"success"} label={label} startIcon={startIcon} />
       </Link>
     );
 }
