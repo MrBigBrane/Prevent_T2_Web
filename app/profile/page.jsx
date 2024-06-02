8fce4bfc45b7e55d68ade6f36fac1722f5186fb5
MrBigBrane <theepicmodafe@gmail.com>
2024-06-01 21:52:51 -0700

    Profile page fixed with grid

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,7 @@
 'use server';
 
 import LinkButton from '@/components/buttons/LinkButton';
-import { Box, Paper, Typography, Grid, colors, IconButton } from '@mui/material';
+import { Box, Paper, Typography, Grid, colors, IconButton, Button } from '@mui/material';
 import MuiAvatar from '@/components/buttons/avatar/MuiAvatar';
 import EmailReset from '../../components/forms/reset/EmailReset';
 import PasswordModal from '../../components/forms/reset/passwordchange/PasswordModal';
@@ -9,6 +9,7 @@ import { createClient } from "@/utils/supabase/server";
 import LaunchIcon from '@mui/icons-material/Launch';
 import NameModal from '../../components/forms/reset/username/NameModal';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 export default async function ProfilePage() {
     const supabase = createClient();
@@ -22,52 +23,59 @@ export default async function ProfilePage() {
     }
 
     return (
-      <Box
-        sx={{
-          width: "100%",
-          justifyContent: "center",
-          display: "inline-flex",
-          margin: "auto",
-          padding: "2rem",
-        }}
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        //   alignItems="center"
+        style={{ height: "100vh" }} // Full viewport height
       >
-        <Box sx={{ width: "100%" }} marginTop={5}>
-          <Paper elevation={10} style={{ width: "45%", marginTop: "2rem" }}>
-            <Box display="flex" padding={5}>
-              <Box display="inline-flex">
-                <IconButton>
-                  <MuiAvatar
-                    name={`${user.user_metadata.first_name} ${user.user_metadata.last_name}`}
-                    style={{ width: 90, height: 90 }}
-                  />
-                </IconButton>
-                <Box display={"inline-flex"} alignSelf={"center"}>
-                  <Typography variant="h4" style={{ textAlign: "center" }}>
-                    {user.user_metadata.first_name}{" "}
-                    {user.user_metadata.last_name}
-                  </Typography>
-                  <NameModal
-                    firstName={user.user_metadata.first_name}
-                    lastName={user.user_metadata.last_name}
-                  />
-                  <EmailReset email={user.user_metadata.email} />
-                </Box>
-                  {/* <Box marginLeft={5}> */}
-                    
-                  {/* </Box> */}
-              </Box>
-            </Box>
-          </Paper>
-        </Box>
-
-        <Box style={{ width: "45%" }} marginTop={5}>
-          <Typography variant="h4" sx={{ marginBottom: "5px" }}>
-            Forms
-          </Typography>
-          <Grid container spacing={3} justifyContent={"center"}>
-            <Grid item xs={6}>
-              <Paper elevation={10}>
-                <Box sx={{ padding: "20px" }}>
+        <Grid container item xs={12} justifyContent="center" marginTop={5}>
+          <Grid item xs={10} sm={10} md={8}>
+            <Paper
+              elevation={10}
+              style={{ padding: 16, textAlign: "center", margin: "auto" }}
+            >
+              <IconButton>
+                <MuiAvatar
+                  name={`${user.user_metadata.first_name} ${user.user_metadata.last_name}`}
+                  style={{ width: 90, height: 90 }}
+                />
+              </IconButton>
+              <Grid display={"flex"} justifyContent={"center"}>
+                <Typography variant="h4" style={{ textAlign: "center" }}>
+                  {user.user_metadata.first_name} {user.user_metadata.last_name}
+                </Typography>
+                <NameModal
+                  firstName={user.user_metadata.first_name}
+                  lastName={user.user_metadata.last_name}
+                />
+              </Grid>
+              <Grid display={"flex"} justifyContent={"center"}>
+                <EmailReset email={user.user_metadata.email} />
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid container item xs={12} justifyContent="center">
+          <Grid item xs={10} sm={10} md={8}>
+            <Typography variant="h4" sx={{ marginBottom: "5px" }}>
+              Forms
+            </Typography>
+            <Grid
+              container
+              item
+              xs={12}
+              spacing={2}
+              justifyContent="center"
+              display={"flex"}
+            >
+              <Grid item xs={6}>
+                <Paper
+                  elevation={10}
+                  style={{ padding: 16, textAlign: "center", margin: "auto" }}
+                >
                   <Typography variant="h5">Onboarding Form</Typography>
                   <Typography variant="body2" marginBottom={2}>
                     Description
@@ -78,42 +86,42 @@ export default async function ProfilePage() {
                     label="Onboarding Form"
                     type=""
                   />
-                </Box>
-              </Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper elevation={10}>
-                <Box sx={{ padding: "20px" }}>
+                </Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper
+                  elevation={10}
+                  style={{ padding: 16, textAlign: "center", margin: "auto" }}
+                >
                   <Typography variant="h5">Coach Registration Form</Typography>
                   <Typography variant="body2" marginBottom={2}>
                     Description
                   </Typography>
-                  <LinkButton
-                    href="/profile/becomecoach"
-                    endIcon={<LaunchIcon />}
-                    label="Become a Coach"
-                    type=""
-                  />
-                </Box>
-              </Paper>
+                  <Link href="/profile/becomecoach">
+                    <Button endIcon={<LaunchIcon />} variant="contained">
+                      Become a Coach
+                    </Button>
+                  </Link>
+                </Paper>
+              </Grid>
             </Grid>
           </Grid>
-        </Box>
-        <Box marginTop={5} width={"45%"}>
-          <Typography variant="h4" sx={{ marginBottom: "5px" }}>
-            Security
-          </Typography>
-          <Box>
-            <Paper elevation={10}>
-              <Box sx={{ padding: "20px" }}>
-                <Typography variant="h5" marginBottom={2}>
-                  Change Password
-                </Typography>
-                <PasswordModal />
-              </Box>
+        </Grid>
+        <Grid container item xs={12} justifyContent="center">
+          <Grid item xs={10} sm={10} md={8}>
+            <Typography variant="h4" sx={{ marginBottom: "5px" }}>
+              Security
+            </Typography>
+            <Grid item xs={12}>
+            <Paper elevation={10} style={{ padding: 16, textAlign: "center", margin: "auto" }}>
+              <Typography variant="h5" marginBottom={2}>
+                Change Password
+              </Typography>
+              <PasswordModal />
             </Paper>
-          </Box>
-        </Box>
-      </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     );
 }
